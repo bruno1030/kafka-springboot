553cb48945fc21b869fdaaaf8fdcb841a915f59b
Bruno <brunomartins1030@gmail.com>
2022-08-07 10:35:17 -0300

    First commit

---
+++ src/main/java/net/javaguides/springbootkafkatutorial/kafka/JsonKafkaProducer.java
@@ -0,0 +1,35 @@
+package net.javaguides.springbootkafkatutorial.kafka;
+
+import net.javaguides.springbootkafkatutorial.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
+
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    public JsonKafkaProducer(KafkaTemplate<String, User> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(User data){
+
+        LOGGER.info(String.format("Message sent -> %s", data.toString()));   // por isso foi necessario pdeir para o Intellij criar o metodo "toString" la na model
+
+        Message<User> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, "javaguides_json")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+
+}
